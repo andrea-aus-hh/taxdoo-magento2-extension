e738d2d7e24434e686e5c14b04686e88d9178447
Andrea Lazzaretti <andrealazzaretti@gmail.com>
2021-06-09 22:47:47 +0200

    Removed two unused methods (inherited from TaxJar) from Model/Configuration.php : setDisplaySettings() and setTaxBasis()

--- Model/Configuration.php
+++ Model/Configuration.php
@@ -103,43 +103,6 @@ class Configuration
     }
 
     /**
-     * Sets tax basis in Magento
-     *
-     * @param string $configJson
-     * @return void
-     */
-    public function setTaxBasis($configJson)
-    {
-        $basis = 'shipping';
-
-        if ($configJson['tax_source'] === 'origin') {
-            $basis = 'origin';
-        }
-
-        $this->_setConfig('tax/calculation/based_on', $basis);
-    }
-
-    /**
-     * Set display settings for tax in Magento
-     *
-     * @return void
-     */
-    public function setDisplaySettings()
-    {
-        $settings = [
-            'tax/display/type',
-            'tax/display/shipping',
-            'tax/cart_display/price',
-            'tax/cart_display/subtotal',
-            'tax/cart_display/shipping'
-        ];
-
-        foreach ($settings as $setting) {
-            $this->_setConfig($setting, 1);
-        }
-    }
-
-    /**
      * Store config
      *
      * @param string $path
